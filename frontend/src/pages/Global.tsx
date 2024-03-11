555a327235d01765e1c3d9b3a5852417586c6927
Simon <19sipe@stockholmscience.se>
2024-03-11 10:32:16 +0100

    Skeleton for pages

--- frontend/src/pages/Global.tsx
+++ frontend/src/pages/Global.tsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+
 export const Global = () => {
-  return <h1>Global</h1>;
+  return (
+    <>
+      <h1>Welcome, [name].</h1>
+
+      {/* Create Group */}
+      <form action="">
+        <input type="text" name="" id="" />
+        <button>Create Room</button>
+      </form>
+
+      {/* Existing Groups */}
+      <div>
+        <button>
+          <Link to="/group">Simon's Room</Link>
+        </button>
+      </div>
+
+      {/* Global Chat */}
+      <div>
+        <div></div>
+        <input type="text" />
+      </div>
+    </>
+  );
 };
--- frontend/src/pages/Group.tsx
+++ frontend/src/pages/Group.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export const Group = () => {
-  return <h1>Group</h1>;
+  return (
+    <>
+      {/* Topside Group */}
+      <div>
+        <button>
+          <Link to="/global">Go back</Link>
+        </button>
+        <div>[Group.name]'s Room</div>
+      </div>
+
+      {/* Group Chat  */}
+      <div>
+        <div></div>
+        <input type="text" />
+      </div>
+    </>
+  );
 };
