e697a957b0c415b63dbc2fbdd00204c5fb061082
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-03-11 15:51:06 +0100

    hard coded messageLIst, interface Imessage etc.

--- frontend/src/components/MessageComponent.tsx
+++ frontend/src/components/MessageComponent.tsx
@@ -1,13 +1,19 @@
-export const MessageComponent = () => {
+import { IMessage } from "../models/IMessage";
+
+interface IMessageProps {
+  message: IMessage;
+}
+
+export const MessageComponent = ({ message }: IMessageProps) => {
   return (
-    <>
-      <div>
-        <span></span>
-        <span></span>
+    <div key={message.id} className=" bg-gray-200 rounded p-3">
+      <div className=" text-xs flex gap-2">
+        <span>{message.author}</span>
+        <span>{message.date}</span>
       </div>
       <div>
-        <span></span>
+        <span>{message.message}</span>
       </div>
-    </>
+    </div>
   );
 };
--- frontend/src/models/Message.ts
+++
@@ -1,8 +0,0 @@
-export class Message {
-  constructor(
-    public id: number,
-    public author: string,
-    public date: Date,
-    public message: string
-  ) {}
-}
