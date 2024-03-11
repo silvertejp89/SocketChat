f7a5790b90fb88249b9c7bdbc6fc1412e2c3ce4b
fika <filipkarlstrom03@gmail.com>
2024-03-11 10:28:15 +0100

    tailwind config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router.tsx";
+import "./main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{tsx,jsx,ts,js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
