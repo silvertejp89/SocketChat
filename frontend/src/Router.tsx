134fc0f6ccbfd0ef020c6e6652447429d621cf6e
Simon <19sipe@stockholmscience.se>
2024-03-11 10:10:20 +0100

    fix2

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -15,7 +15,7 @@ export const router = createBrowserRouter([
         index: true,
       },
       {
-        path: "/animals",
+        path: "/Global",
         element: <Global />,
       },
       {
