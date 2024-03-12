d47d7d24740487e8fa5815acf80a689525ab05d7
Fika03 <filipkarlstrom03@gmail.com>
2024-03-12 10:03:04 +0100

    backend fixes

---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { User } from "./models/User";
+
+const users: User[] = [];
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected");
+
+  socket.on("add_user", (newUser: User) => {
+    users.push(newUser);
+    console.log(users);
+    io.emit("users_updated", users);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is up and running");
+});
